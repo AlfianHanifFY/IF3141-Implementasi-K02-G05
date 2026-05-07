f3a08545d0a91f78082a0d7faebaae72bd0ef1f8
DaDecky <mdikiisra98@gmail.com>
2026-05-08 04:10:37 +0700

    feat(monitor): scaffold monitoring addon

---
+++ custom_addons/monitor/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Monitor',
+    'version': '17.0.1.0.0',
+    'category': 'Monitoring',
+    'summary': 'Monitoring Umum',
+    'author': 'Custom',
+    'license': 'LGPL-3',
+    'depends': ['base','client_prospect','dokumen'],
+    'data': [],
+    'application': True,
+    'installable': True,
+}
